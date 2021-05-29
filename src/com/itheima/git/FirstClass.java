2cfc5272a341b99f1468ac3e065df3e5278d4d8c
weixiaoyu <mllyy1227@163.com>
2021-05-29 13:27:28 +0800

    本地工程添加到git

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("HELLO  gei---------");
+        System.out.println("HELLO  gei---------");
+        System.out.println("HELLO  gei---------");
+        System.out.println("HELLO  gei---------");
+        System.out.println("HELLO  gei---------");
+        System.out.println("HELLO  gei---------");
+    }
+}
